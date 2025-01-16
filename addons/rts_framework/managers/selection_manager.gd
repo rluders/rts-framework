1dedaf5df25800293d556ade95e999ba77a45814
Ricardo Lüders <ricardo@luders.com.br>
2025-01-16 23:07:33 +0100

    feat: selection box

--- addons/rts_framework/managers/selection_manager.gd
+++ addons/rts_framework/managers/selection_manager.gd
@@ -10,28 +10,48 @@ var start_sel_pos: Vector2 = Vector2.ZERO
 var end_sel_pos: Vector2 = Vector2.ZERO
 var is_dragging: bool = false
 
+func _ready() -> void:
+	if selection_box:
+		selection_box.visible = false
+
+func get_mouse_position() -> Vector2:
+	return get_viewport().get_mouse_position()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("select"):
+		start_selection(get_mouse_position())
+	elif Input.is_action_just_released("select"):
+		end_selection(get_mouse_position())
+
+	if is_dragging:
+		update_selection(get_viewport().get_mouse_position())
+
 func start_selection(start_pos: Vector2) -> void:
 	start_sel_pos = start_pos
 	is_dragging = true
+
 	selection_box.visible = true
 	selection_box.position = start_pos
 	selection_box.size = Vector2.ZERO
-
-func update_selection(current_pos: Vector2) -> void:
-	if is_dragging:
-		end_sel_pos = current_pos
-		var rect = Rect2(start_sel_pos, end_sel_pos - start_sel_pos).abs()
-		selection_box.position = rect.position
-		selection_box.size = rect.size
 
 func end_selection(end_pos: Vector2) -> void:
 	if is_dragging:
 		is_dragging = false
 		selection_box.visible = false
 		end_sel_pos = end_pos
+		
 		var rect = Rect2(start_sel_pos, end_sel_pos - start_sel_pos).abs()
 		selected_units = get_units_in_rect(rect)
+
 		emit_signal("units_selected", selected_units)
+
+func update_selection(current_pos: Vector2) -> void:
+	if is_dragging:
+		end_sel_pos = current_pos
+
+		var rect = Rect2(start_sel_pos, end_sel_pos - start_sel_pos).abs()
+		selection_box.position = rect.position
+		selection_box.size = rect.size
 
 func deselect_all() -> void:
 	selected_units.clear()
