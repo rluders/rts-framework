a12ebadcd6bbfcc4579a0d16c1e8cc9abd2acba0
Ricardo Lüders <ricardo@luders.com.br>
2025-01-20 19:50:57 +0100

    feat: StateMachine::get_state helper

--- addons/rts_framework/scripts/states/state_machine.gd
+++ addons/rts_framework/scripts/states/state_machine.gd
@@ -41,3 +41,6 @@ func _on_state_transitioned(state: State, new_state_name: String) -> void:
 	current_state = new_state
 	
 	state_changed.emit(new_state_name.to_lower())
+
+func get_state(state_name: String) -> State:
+	return states.get(state_name)
