c99c48ac0544f2cabb24706afcd6236718a84950
Ricardo Lüders <ricardo@luders.com.br>
2025-02-14 10:56:04 +0100

    fix: clean up signals singleton for now

--- addons/rts_framework/scripts/core/signals.gd
+++ addons/rts_framework/scripts/core/signals.gd
@@ -1,12 +1,3 @@
 extends Node
 
 # This needs to be registered as global singleton `Signals`
-
-signal units_selected(units: Array)
-signal command_issued(command: String, units: Array, target: Variant, context: Dictionary)
-
-func emit_units_selected(units: Array) -> void:
-	units_selected.emit(units)
-
-func emit_command_issued(command: String, units: Array, target: Variant, context: Dictionary) -> void:
-	command_issued.emit(command, units, target, context)
