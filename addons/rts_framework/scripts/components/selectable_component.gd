715a4c6f5716debbeea643b11eeaf0ab92cb80a7
Ricardo Lüders <ricardo@luders.com.br>
2025-02-14 10:52:57 +0100

    fix: remove signal from selection manager

--- addons/rts_framework/scripts/components/selectable_component.gd
+++ addons/rts_framework/scripts/components/selectable_component.gd
@@ -8,19 +8,20 @@ signal deselected
 @export var selection_sprite: Sprite3D
 @export var highlight_color: Color = Color(1, 1, 0, 1)
 
+var is_selected : bool = false
+
 func _ready() -> void:
 	assert(root_entity != null, "root_entity must be set for SelectableComponent")
 	
 	if selection_sprite:
 		selection_sprite.visible = false
-	
-	# Connect to global signals
-	Signals.units_selected.connect(_on_units_selected)
 
 func select() -> void:
 	if selection_sprite:
 		selection_sprite.visible = true
 		set_selection_color(highlight_color)
+	
+	is_selected = true
 	
 	selected.emit()
 
@@ -28,15 +29,16 @@ func deselect() -> void:
 	if selection_sprite:
 		selection_sprite.visible = false
 	
+	is_selected = false
+	
 	deselected.emit()
+
+func toggle_selection() -> void:
+	if is_selected:
+		deselect()
+	else:
+		select()
 
 func set_selection_color(color: Color) -> void:
 	if selection_sprite and selection_sprite.modulate:
 		selection_sprite.modulate = highlight_color
-
-# --- Signal Handlers ---
-func _on_units_selected(units: Array) -> void:
-	if root_entity in units:
-		select()
-	else:
-		deselect()
