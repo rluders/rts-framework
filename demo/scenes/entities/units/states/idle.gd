5ce89f6c9b60ef66c406251f6284023d85a51872
Ricardo Lüders <ricardo@luders.com.br>
2025-02-16 19:34:18 +0100

    style: remove comments to myself

--- demo/scenes/entities/units/states/idle.gd
+++ demo/scenes/entities/units/states/idle.gd
@@ -2,12 +2,9 @@ extends State
 
 func enter(params: Dictionary = {}) -> void:
 	print("Entering Idle State")
-	# Aqui, você pode configurar animações ou efeitos visuais para o estado ocioso
 
 func exit() -> void:
 	print("Exiting Idle State")
-	# Limpar ou preparar ao sair do estado ocioso
 
 func update(delta: float) -> void:
-	# Atualizações lógicas para o estado ocioso, caso necessário
 	pass
