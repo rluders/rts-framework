d4ad96881c6c43c7bb7103f3277c456365d787ef
sami.nosatzki <sami.nosatzki@gmail.com>
2025-03-08 03:56:58 +0200

    Add auto-redraw for dynamic_circle_2d. Change the calculation of alpha_from_margin from hardcoded values to constant in  detailed_fog_of_war shader

--- addons/rts_framework/features/vision/dynamic_circle_2d.gd
+++ addons/rts_framework/features/vision/dynamic_circle_2d.gd
@@ -1,9 +1,15 @@
 @tool
 extends Node2D
 
-@export var color : Color = Color.WHITE
-@export var radius : int = 15
+@export var color : Color = Color.WHITE:
+	set(value):
+		color = value
+		queue_redraw()
 
+@export var radius : int = 15:
+	set(value):
+		radius = value
+		queue_redraw()
 
 func _draw():
 	draw_circle(Vector2(0, 0), radius, color)
