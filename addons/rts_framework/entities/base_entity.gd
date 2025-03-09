d959e5a5d50673535219155ab2d923287bdfccea
Amit Sami Nosatzki <42678113+amitnos123@users.noreply.github.com>
2025-03-10 00:12:56 +0200

    feat: fog of war (#27)
    * The soldier can move
    
    * Made a for of war mechanism based on project godot-open-rts made by lampe-games.
    
    * checking from fog-of-war branch to main branch
    
    * Revert "checking from fog-of-war branch to main branch"
    
    This reverts commit 63ba6ea37f34198e5eb74471d0879ed30bf49fb1.
    
    * fixing problems I made
    
    * fixing problems I made
    
    * Revert "fix conflict"
    
    This reverts commit aba2e80b478c9b79f27da41c7a6eab9710ad0004, reversing
    changes made to 5caef79f7497ba39b144702a5d58e03ce5536044.
    
    * fixed basic merge and conflict problems
    
    * tested fog of war
    
    * Revert "fixed basic merge and conflict problems"
    
    This reverts commit 888b2b1897405888f789ed229086d8f94d5862b6, reversing
    changes made to e01a8ac6c1ac54d75c85402e4a92ce202059b17a.
    
    * fog of war works
    
    * moved files, according to PR and #25
    
    * moved files, according to PR and #25
    
    * Changed file according to PR. I changed the code to be the same as in main repository.
    
    * fixed errors
    
    * rename DynamicCircle2D to dynamic_circle_2d
    
    * Forgot to update the name at fog_of_war_manager.gd
    
    * deleted demo/scenes/entities/units/soldier/ directory.
    
    * moved fog_of_war_manager to vision and made it it's own scene
    
    * I added all the values I thought were relevant to  node itself.
    I also added placeholders with TODO for description of all the values
    
    * Added descriptions for export variables
    
    * Added to FogOfWarManager Node exported values of Editor Only Circle
    
    * Added icon for fog of war manager class
    
    * update icon
    
    * Added that fog can copy a mesh node' size. For automation of fog size equal to map size. Added the ability to change debug texture view size. Accepts values between 0 to 1.
    
    * Add auto-redraw for dynamic_circle_2d. Change the calculation of alpha_from_margin from hardcoded values to constant in  detailed_fog_of_war shader
    
    * Fixed a detailed_fog_of_war shader according to bot PR

--- addons/rts_framework/entities/base_entity.gd
+++ addons/rts_framework/entities/base_entity.gd
@@ -4,6 +4,8 @@ class_name BaseEntity
 @export var team: int = 0
 @export var is_active: bool = true
 
+@export var sight_range : int = 2
+
 func _ready() -> void:
 	print_debug("Calling framework BaseEntity::_ready")
 	add_to_group("entities")
@@ -12,10 +14,12 @@ func get_component(component_class: String) -> Node:
 	if has_node("Components"):
 		var components = $Components.get_children()
 		for component in components:
-			# FIXME it would nice to check it with get_class() but it always get Node
-			if component.name == component_class + "Component":
+			if component is BaseComponent:
 				return component
 	return null
 
 func has_component(component_class: String) -> bool:
 	return true if get_component(component_class) else false
+
+func is_revealing() -> bool:
+	return true;
