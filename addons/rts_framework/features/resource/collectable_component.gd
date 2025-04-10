c832d221104735095fc47621123895599d48a276
sami.nosatzki <sami.nosatzki@gmail.com>
2025-04-11 01:06:35 +0300

    Updated collectable_component.gd according to PR

--- addons/rts_framework/features/resource/collectable_component.gd
+++ addons/rts_framework/features/resource/collectable_component.gd
@@ -6,21 +6,21 @@ signal on_collected(amount: int)
 ## Emitted when resources run out.
 signal on_depleted()
 
-## AAA
+## Type of resource this component manages (gold or wood)
 @export_enum("gold", "wood") var resource_type
 ## AAA
 @export_range(0, 1000, 1, "or_greater") var resource_amount : int = 1000 :
 	set(value):
-		self.resource_amount = value
-		if self.resource_amount <= 0:
-			self.resource_amount = 0
+		resource_amount = value
+		if resource_amount <= 0:
+			resource_amount = 0
 			emit_signal("on_depleted")
-## AAA
-var is_depleted : bool : 
+## Whether the resource has been completely depleted (read-only)
+var  is_depleted : bool : 
 	set(value):
 		return # Can't set
 	get:
-		return (resource_amount > 0)
+		return (resource_amount <= 0)
 	
 
 func _ready() -> void:
@@ -28,6 +28,8 @@ func _ready() -> void:
 
 # Reduces resource amount and returns the actual collected amount.
 func collect(amount: int) -> int:
+	if amount <= 0:
+		return 0
 	if amount > self.resource_amount:
 		var real_amount = resource_amount
 		self.resource_amount = 0
@@ -35,6 +37,6 @@ func collect(amount: int) -> int:
 	self.resource_amount = self.resource_amount - amount
 	return amount
 
-# Sets resource amount to 0 and emits on_depleted.
-func repair(amount: int) -> void:
+# Completely depletes the resource.
+func deplete(amount: int) -> void:
 	self.resource_amount = 0
