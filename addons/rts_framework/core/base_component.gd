8a28894e61d69ffe2ac86742d80cf11b1b20c15f
sami.nosatzki <sami.nosatzki@gmail.com>
2025-04-12 19:59:43 +0300

    Added warning, when owner of component is not BaseEntity

--- addons/rts_framework/core/base_component.gd
+++ addons/rts_framework/core/base_component.gd
@@ -1,2 +1,8 @@
 extends Node
 class_name BaseComponent
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings : PackedStringArray = PackedStringArray([])
+	if owner is not BaseEntity:
+		warnings.append("Component's Owner is not BaseEntity")
+	return warnings
